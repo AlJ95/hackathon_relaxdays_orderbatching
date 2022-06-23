da3efd38db3ac2416bb83f40e299361e1a43dd6b
AlJ95 <jan.albrecht1995@googlemail.com>
2022-06-23 21:16:59 +0200

    read json

--- orderbatching/main.py
+++ orderbatching/main.py
@@ -3,3 +3,7 @@ import json
 if __name__ == "__main__":
     with open("instance.json") as file:
         data = json.load(file)
+
+    article_locations = data["ArticleLocations"]
+    orders = data["Orders"]
+    articles = data["Articles"]
