6ac304e30f947d12dd2ba06a0c96424e4e37f5ba
Raaaaaid <52356233+Raaaaaid@users.noreply.github.com>
2022-06-23 22:40:11 +0200

    datastructures repr

--- orderbatching/datastructures.py
+++ orderbatching/datastructures.py
@@ -7,6 +7,12 @@ class Article:
         self.warehouse_id = None
         self.aisle_id = None
 
+    def __repr__(self):
+        return (
+            f'<Article article_id={self.article_id} volume={self.volume} '
+            f'warehouse_id={self.warehouse_id} aisle_id={self.aisle_id}>'
+        )
+
 
 class Order:
 
@@ -19,6 +25,12 @@ class Order:
                 self._weight_per_warehouse[article.warehouse_id] += article.volume
             except KeyError:
                 self._weight_per_warehouse[article.warehouse_id] = article.volume
+
+    def __repr__(self):
+        return (
+            f'<Order order_id={self.order_id} '
+            f'articles=[{" ,".join([str(article) for article in self.articles])}]>'
+        )
 
     def get_warehouse_weight(self, warehouse_id: int) -> int:
         return self._weight_per_warehouse.get(warehouse_id, 0)
