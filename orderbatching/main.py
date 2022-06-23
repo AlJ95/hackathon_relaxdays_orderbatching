80e717beab99c5c60a77b132e3d10db60df1aa6e
AlJ95 <jan.albrecht1995@googlemail.com>
2022-06-23 21:34:30 +0200

    init skeleton

--- orderbatching/main.py
+++ orderbatching/main.py
@@ -1,7 +1,17 @@
 import json
 
+
+def get_location(data, article_id):
+    article = [article for article in data if article["article_id"] == article_id][-1]
+    return article["Warehouse"], article["Aisle"]
+
+
+def get_weight():
+    pass
+
+
 if __name__ == "__main__":
-    with open("instance.json") as file:
+    with open("data/instance.json") as file:
         data = json.load(file)
 
     article_locations = data["ArticleLocations"]
