981f8e56285aa8c2f9180f0e4dfff934bb867d22
AlJ95 <jan.albrecht1995@googlemail.com>
2022-06-25 01:31:15 +0200

    program runs and produces solution in ~50seconds

--- orderbatching/main.py
+++ orderbatching/main.py
@@ -7,8 +7,8 @@ import os
 
 def main(argv):
 
-    instance_path = argv[1] if len(argv) > 1 else f"{os.path.dirname(__file__)}/data/instance4.json"
-    solution_path = argv[2] if len(argv) > 2 else f"{os.path.dirname(__file__)}/data/solution4.json"
+    instance_path = argv[1] if len(argv) > 1 else f"{os.path.dirname(__file__)}/data/instance3.json"
+    solution_path = argv[2] if len(argv) > 2 else f"{os.path.dirname(__file__)}/data/solution3.json"
 
     solution_dir, _ = os.path.split(solution_path)
     if solution_dir and not os.path.isdir(solution_dir):
