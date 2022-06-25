3727d8a7bfae697cb95f485b2e2234a697b74055
Raaaaaid <52356233+Raaaaaid@users.noreply.github.com>
2022-06-25 11:51:07 +0200

    document datastructures.py

--- marketsimulation/main.py
+++ marketsimulation/main.py
@@ -1,5 +1,7 @@
 from api_handler import API
 
+# TODO: Readme
+
 if __name__ == "__main__":
     articles = requests.get(f"{API_URL}{GET_ALL_ARTICLES}")
     player = requests.get(f"{API_URL}{GET_PLAYERS}")
--- orderbatching/datastructures.py
+++ orderbatching/datastructures.py
@@ -11,6 +11,10 @@ class BatchLimitExceeded(Exception):
 
 
 class Article:
+    """
+    This class is used to represent an article. For every article it holds its article_id, volume, warehouse_id and
+    aisle_id.
+    """
 
     def __init__(self, article_id: int, volume: int):
         self.article_id = article_id
@@ -26,6 +30,11 @@ class Article:
 
 
 class Order:
+    """
+    This class is used to represent an order. For every order it holds its order_id, articles, warehouse_ids occuring
+    in this order and a warehouse_bit_vector_repr (see get_warehouse_bit_vector_repr). Furthermore it has the class
+    attribute all_warehouse_ids to save all the existing warehouse_ids of a specific problem instance.
+    """
 
     all_warehouse_ids = set()
 
@@ -42,7 +51,13 @@ class Order:
             f'articles=[{", ".join([str(article) for article in self.articles])}]>'
         )
 
-    def get_warehouse_bit_vector_repr(self):
+    def get_warehouse_bit_vector_repr(self) -> gmpy2.mpz:
+        """
+        Creates a bitvector with 1s for each warehouse which has to visited in this order. This binary
+        representation is then converted to an integer and saved in a gmpy2.mpz.
+
+        :return: gmpy2.mpz representation of the bitvector
+        """
         if self.warehouse_bit_vector_repr is None:
             bit_list = [(1 if i in self.warehouse_ids else 0) for i in Order.all_warehouse_ids]
             self.warehouse_bit_vector_repr = gmpy2.mpz(int(''.join([str(i) for i in bit_list]), 2))
@@ -50,11 +65,20 @@ class Order:
 
     @classmethod
     def cast_all_warehouse_ids_attr(cls):
-        # to preserve order when iterating over all_warehouse_ids
+        """
+        After all orders are instantiated, all_warehouse_ids (set) holds all existing warehouse_ids. This set
+        is then casted to a list so that a specific order is preserved when iterating over all_warehouse_ids.
+        (Iterating over sets is arbitrary.)
+        """
         cls.all_warehouse_ids = list(cls.all_warehouse_ids)
 
 
 class Wave:
+    """
+    This class is used to represent a wave. For every wave it holds its unique wave_id, article_amount, wave_size,
+    orders (set Order instances) and batch_ids. The class attribute id_counter is used to ensure a unique id for every
+    new instance.
+    """
 
     id_counter = 0
 
@@ -73,6 +97,12 @@ class Wave:
         )
 
     def add(self, order: Order):
+        """
+        Adds an order to the wave and raises WaveLimitExceeded if this order does not fit into the wave. It also
+        keeps track of total articles in this wave.
+
+        :param order: order to add to the wave
+        """
         order_article_amount = len(order.articles)
         if self.article_amount + order_article_amount <= self.wave_size:
             self.article_amount += order_article_amount
@@ -80,7 +110,10 @@ class Wave:
         else:
             raise WaveLimitExceeded
 
-    def get_solution_dict(self):
+    def get_solution_dict(self) -> dict:
+        """
+        :return: a dict which holds a representation of the wave corresponding to the specified solution format
+        """
         return {
             "WaveId": self.wave_id,
             "BatchIds": self.batch_ids,
@@ -90,6 +123,11 @@ class Wave:
 
 
 class Batch:
+    """
+    This class is used to represent a batch. For every batch it holds its batch_id, max_batch_volume, volume and
+    items ((article, order_id) tuples). The class attribute id_counter is used to ensure a unique id for every new
+    instance.
+    """
 
     id_counter = 0
 
@@ -107,6 +145,13 @@ class Batch:
         )
 
     def add(self, article: Article, order_id: int):
+        """
+        Adds an (article, order_id)-tuple to the batch and raises BatchLimitExceeded if this article does not fit into
+        the batch. It also keeps track of total volume of this batch.
+
+        :param article: article to add to the batch
+        :param order_id: order_id from which the article is from
+        """
         article_volume = article.volume
         if self.volume + article_volume <= self.max_batch_volume:
             self.volume += article_volume
@@ -114,7 +159,10 @@ class Batch:
         else:
             raise BatchLimitExceeded
 
-    def get_solution_dict(self):
+    def get_solution_dict(self) -> dict:
+        """
+        :return: a dict which holds a representation of the batch corresponding to the specified solution format
+        """
         item_list = list(self.items)
         item_list.sort(key=lambda item: (item[0], item[1]))
         item_list = [{"OrderId": item[1], "ArticleId": item[0]} for item in item_list]
